8d0640e1ce382f85046cc1a0f8e35d33318004db
Akash99 <77772160+akasharekar@users.noreply.github.com>
2024-04-16 17:46:06 +0530

    pricing page add css and js for side var click scorlling

--- wwwroot/js/pricing.js
+++ wwwroot/js/pricing.js
@@ -1,47 +1,55 @@
 
 
-document.querySelector(".Net-show").addEventListener("click", () => {
-    let elements = document.getElementsByClassName("lst-1");
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].style.display === "block") {
-            elements[i].style.display = "none";
-        } else {
-            elements[i].style.display = "block";
+// side bar js
+window.onload = function() { 
+    console.log("Page loaded");
+
+    document.querySelector(".Net-show").addEventListener("click", () => {
+        let elements = document.getElementsByClassName("lst-1");
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].style.display === "block") {
+                elements[i].style.display = "none";
+            } else {
+                elements[i].style.display = "block";
+            }
         }
-    }
-});
-
-
-document.querySelector(".s-s-show").addEventListener("click", () => {
-    let elements = document.getElementsByClassName("lst-2");
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].style.display === "block") {
-            elements[i].style.display = "none";
-        } else {
-            elements[i].style.display = "block";
+    });
+    
+    
+    document.querySelector(".s-s-show").addEventListener("click", () => {
+        let elements = document.getElementsByClassName("lst-2");
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].style.display === "block") {
+                elements[i].style.display = "none";
+            } else {
+                elements[i].style.display = "block";
+            }
         }
-    }
-});
-
-
-document.querySelector(".sec-show").addEventListener("click", () => {
-    let elements = document.getElementsByClassName("lst-3");
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].style.display === "block") {
-            elements[i].style.display = "none";
-        } else {
-            elements[i].style.display = "block";
+    });
+    
+    
+    document.querySelector(".sec-show").addEventListener("click", () => {
+        let elements = document.getElementsByClassName("lst-3");
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].style.display === "block") {
+                elements[i].style.display = "none";
+            } else {
+                elements[i].style.display = "block";
+            }
         }
-    }
-});
-
-document.querySelector(".cs-show").addEventListener("click", () => {
-    let elements = document.getElementsByClassName("lst-4");
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i].style.display === "block") {
-            elements[i].style.display = "none";
-        } else {
-            elements[i].style.display = "block";
+    });
+    
+    document.querySelector(".cs-show").addEventListener("click", () => {
+        let elements = document.getElementsByClassName("lst-4");
+        for (let i = 0; i < elements.length; i++) {
+            if (elements[i].style.display === "block") {
+                elements[i].style.display = "none";
+            } else {
+                elements[i].style.display = "block";
+            }
         }
-    }
-});
+    });
+}
+
+
+
