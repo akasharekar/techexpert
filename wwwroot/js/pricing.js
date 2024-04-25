c7975587bde3a0824da164e05af6631ce2d5f88b
Akash99 <77772160+akasharekar@users.noreply.github.com>
2024-04-25 17:43:55 +0530

    just added blue logo in pricing page as said by vishal

--- wwwroot/js/pricing.js
+++ wwwroot/js/pricing.js
@@ -1,55 +1,69 @@
 
 
-// side bar js
-window.onload = function() { 
-    console.log("Page loaded");
+// // side bar js
+// window.onload = function() { 
+//     console.log("Page loaded");
 
-    document.querySelector(".Net-show").addEventListener("click", () => {
-        let elements = document.getElementsByClassName("lst-1");
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[i].style.display === "block") {
-                elements[i].style.display = "none";
-            } else {
-                elements[i].style.display = "block";
-            }
-        }
-    });
+//     document.querySelector(".Net-show").addEventListener("click", () => {
+//         let elements = document.getElementsByClassName("lst-1");
+//         for (let i = 0; i < elements.length; i++) {
+//             if (elements[i].style.display === "block") {
+//                 elements[i].style.display = "none";
+//             } else {
+//                 elements[i].style.display = "block";
+//             }
+//         }
+//     });
     
     
-    document.querySelector(".s-s-show").addEventListener("click", () => {
-        let elements = document.getElementsByClassName("lst-2");
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[i].style.display === "block") {
-                elements[i].style.display = "none";
-            } else {
-                elements[i].style.display = "block";
-            }
-        }
-    });
+//     document.querySelector(".s-s-show").addEventListener("click", () => {
+//         let elements = document.getElementsByClassName("lst-2");
+//         for (let i = 0; i < elements.length; i++) {
+//             if (elements[i].style.display === "block") {
+//                 elements[i].style.display = "none";
+//             } else {
+//                 elements[i].style.display = "block";
+//             }
+//         }
+//     });
     
     
-    document.querySelector(".sec-show").addEventListener("click", () => {
-        let elements = document.getElementsByClassName("lst-3");
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[i].style.display === "block") {
-                elements[i].style.display = "none";
-            } else {
-                elements[i].style.display = "block";
-            }
-        }
-    });
+//     document.querySelector(".sec-show").addEventListener("click", () => {
+//         let elements = document.getElementsByClassName("lst-3");
+//         for (let i = 0; i < elements.length; i++) {
+//             if (elements[i].style.display === "block") {
+//                 elements[i].style.display = "none";
+//             } else {
+//                 elements[i].style.display = "block";
+//             }
+//         }
+//     });
     
-    document.querySelector(".cs-show").addEventListener("click", () => {
-        let elements = document.getElementsByClassName("lst-4");
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[i].style.display === "block") {
-                elements[i].style.display = "none";
-            } else {
-                elements[i].style.display = "block";
-            }
-        }
-    });
-}
+//     document.querySelector(".cs-show").addEventListener("click", () => {
+//         let elements = document.getElementsByClassName("lst-4");
+//         for (let i = 0; i < elements.length; i++) {
+//             if (elements[i].style.display === "block") {
+//                 elements[i].style.display = "none";
+//             } else {
+//                 elements[i].style.display = "block";
+//             }
+//         }
+//     });
+// }
 
 
 
+// second design js code
+
+function scrollToContent() {
+    var contentDiv = document.getElementById("animatedDiv");
+    contentDiv.scrollIntoView({ behavior: 'auto' });
+  }
+
+//   function scrollToContent() {
+//     var contentDiv = document.getElementById("animatedDiv");
+//     window.scrollTo({
+//       top: contentDiv.offsetTop,
+//       behavior: 'auto' // or 'instant' if you prefer
+//     });
+//   }
